9c688ce0895bdd290cc8621b1cd6e5110a250a65
Luuk Wester <luuk.wester@gmail.com>
2025-04-20 11:43:04 +0200

    add ignored deserialization test

--- facet-json/tests/integration/read.rs
+++ facet-json/tests/integration/read.rs
@@ -936,3 +936,28 @@ fn test_str_escaped() {
     let result_ok: Result<S, _> = from_str(json_ok);
     assert_eq!(&result_ok.unwrap().foo, "\"\\abc");
 }
+
+#[test]
+#[ignore]
+fn test_deserialize_tuple() {
+    let result: Result<(&str, i32), _> = from_str(r#"["aaa",3]"#);
+    let ok = result.unwrap();
+    assert_eq!(ok.0, "aaa");
+    assert_eq!(ok.1, 3);
+
+    #[derive(facet::Facet)]
+    struct TestTuple(i32, String, bool);
+    let result: Result<TestTuple, _> = from_str(r#"[3,"aaa",true]"#);
+    let ok = result.unwrap();
+    assert_eq!(ok.0, 3);
+    assert_eq!(ok.1, "aaa");
+    assert!(ok.2);
+}
+
+#[test]
+fn test_deserialize_list() {
+    let result: Result<Vec<i32>, _> = from_str(r#"[1,3]"#);
+    let ok = result.unwrap();
+    assert_eq!(ok[0], 1);
+    assert_eq!(ok[1], 3);
+}
