f58bebf600c097fe38d2ee5f446c4d3e52acb2b2
Amos Wenger <amos@bearcove.eu>
2025-05-11 08:51:53 +0200

    Allow inlining iset methods

--- facet-reflect/src/wip/iset.rs
+++ facet-reflect/src/wip/iset.rs
@@ -11,6 +11,7 @@ impl ISet {
     pub const MAX_INDEX: usize = 63;
 
     /// Creates a new ISet with all (given) fields set.
+    #[inline]
     pub fn all(fields: &[Field]) -> Self {
         let mut iset = ISet::default();
         for (i, _field) in fields.iter().enumerate() {
@@ -20,6 +21,7 @@ impl ISet {
     }
 
     /// Sets the bit at the given index.
+    #[inline]
     pub fn set(&mut self, index: usize) {
         if index >= 64 {
             panic!("ISet can only track up to 64 fields. Index {index} is out of bounds.");
@@ -28,6 +30,7 @@ impl ISet {
     }
 
     /// Unsets the bit at the given index.
+    #[inline]
     pub fn unset(&mut self, index: usize) {
         if index >= 64 {
             panic!("ISet can only track up to 64 fields. Index {index} is out of bounds.");
@@ -36,6 +39,7 @@ impl ISet {
     }
 
     /// Checks if the bit at the given index is set.
+    #[inline]
     pub fn has(&self, index: usize) -> bool {
         if index >= 64 {
             panic!("ISet can only track up to 64 fields. Index {index} is out of bounds.");
@@ -44,6 +48,7 @@ impl ISet {
     }
 
     /// Checks if all bits up to the given count are set.
+    #[inline]
     pub fn are_all_set(&self, count: usize) -> bool {
         if count > 64 {
             panic!("ISet can only track up to 64 fields. Count {count} is out of bounds.");
@@ -53,11 +58,13 @@ impl ISet {
     }
 
     /// Checks if any bit in the ISet is set.
+    #[inline]
     pub fn is_any_set(&self) -> bool {
         self.flags != 0
     }
 
     /// Clears all bits in the ISet.
+    #[inline]
     pub fn clear(&mut self) {
         self.flags = 0;
     }
