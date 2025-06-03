85b6a097e05cd51acf06051c12dc33ba7b6e1a21
Amos Wenger <amos@bearcove.eu>
2025-06-03 09:52:33 +0200

    Test PR

--- facet-core/src/macros.rs
+++ facet-core/src/macros.rs
@@ -103,12 +103,12 @@ macro_rules! value_vtable {
                     if $crate::spez::impls!($type_name: core::marker::Unpin) {
                         traits = traits.union($crate::MarkerTraits::UNPIN);
                     }
-                    if $crate::spez::impls!($type_name: core::panic::UnwindSafe) {
-                        traits = traits.union($crate::MarkerTraits::UNWIND_SAFE);
-                    }
-                    if $crate::spez::impls!($type_name: core::panic::RefUnwindSafe) {
-                        traits = traits.union($crate::MarkerTraits::REF_UNWIND_SAFE);
-                    }
+                    // if $crate::spez::impls!($type_name: core::panic::UnwindSafe) {
+                    //     traits = traits.union($crate::MarkerTraits::UNWIND_SAFE);
+                    // }
+                    // if $crate::spez::impls!($type_name: core::panic::RefUnwindSafe) {
+                    //     traits = traits.union($crate::MarkerTraits::REF_UNWIND_SAFE);
+                    // }
 
                     traits
                 })
