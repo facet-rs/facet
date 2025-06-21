ce188d6c79bf478b2ed631060877347ae2eeae9c
TheLazyDutchman <jjpouwels2004@gmail.com>
2025-06-21 12:30:29 +0200

    Trace shapes for sequence diffs, and support vecs

--- facet-diff/examples/vecs.rs
+++ facet-diff/examples/vecs.rs
@@ -6,4 +6,23 @@ fn main() {
 
     let diff = a.diff(&b);
     println!("{diff}");
+
+    let b = b.as_slice();
+    let a = &a;
+    let diff = a.diff(&b);
+    println!("{diff}");
+
+    let a = vec![1, 2, 3, 4];
+    let b = vec![4, 5, 6, 7];
+    let diff = a.diff(&b);
+    println!("{diff}");
+
+    let b = [5, 6, 4, 7];
+    let diff = a.diff(&b);
+    println!("{diff}");
+
+    let a = (1, 2, 3);
+    let b = (2, 3, 1, 4);
+    let diff = a.diff(&b);
+    println!("{diff}");
 }
