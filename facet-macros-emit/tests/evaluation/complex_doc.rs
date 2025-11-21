809210febc0ccd0c51a5869a5366523a95c63d5d
TeamDman <TeamDman9201@gmail.com>
2025-11-21 18:10:03 -0500

    add more symbols to doc string

--- facet-macros-emit/tests/evaluation/complex_doc.rs
+++ facet-macros-emit/tests/evaluation/complex_doc.rs
@@ -11,6 +11,7 @@ enum Complex {
     /// 3. Backslashes: \\ (single backslash) and \\\\ (double backslash)
     /// 4. Mixed: \\" (backslash quote) and \\\" (backslash escaped quote)
     /// 5. Trailing backslash: \\
+    /// 6. ASCII symbols: ! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~
     #[expect(unused)]
     Variant,
 }
