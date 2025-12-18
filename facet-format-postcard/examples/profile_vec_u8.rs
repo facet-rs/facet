a65442c1353adeec3f0f876b7f7b60abbb7a51dd
Amos Wenger <amos@bearcove.eu>
2025-12-18 13:10:29 +0100

    profiling memcpy is fun

---
+++ facet-format-postcard/examples/profile_vec_u8.rs
@@ -0,0 +1,39 @@
+//! Profile vec_u8 parsing
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let which = args.get(1).map(|s| s.as_str()).unwrap_or("facet");
+    let size: usize = args.get(2).and_then(|s| s.parse().ok()).unwrap_or(0);
+    let iterations: usize = args.get(3).and_then(|s| s.parse().ok()).unwrap_or(100000);
+
+    // Generate test data
+    let data: Vec<u8> = (0..size).map(|i| (i % 256) as u8).collect();
+    let encoded = postcard::to_allocvec(&data).unwrap();
+
+    eprintln!(
+        "Running {} for {} iterations, size={}",
+        which, iterations, size
+    );
+
+    match which {
+        "facet" => {
+            for _ in 0..iterations {
+                let _: Vec<u8> = std::hint::black_box(
+                    facet_format_postcard::from_slice(std::hint::black_box(&encoded)).unwrap(),
+                );
+            }
+        }
+        "postcard" => {
+            use serde::Deserialize;
+            #[derive(Deserialize)]
+            struct B(#[serde(with = "serde_bytes")] Vec<u8>);
+
+            for _ in 0..iterations {
+                let _: B = std::hint::black_box(
+                    postcard::from_bytes(std::hint::black_box(&encoded)).unwrap(),
+                );
+            }
+        }
+        _ => eprintln!("Usage: profile_vec_u8 [facet|postcard] [size] [iterations]"),
+    }
+}
