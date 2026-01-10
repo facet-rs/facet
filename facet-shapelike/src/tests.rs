70f857e7ed12e25c9f61c29c4c37421eb8f2822a
Amos Wenger <amos@bearcove.eu>
2026-01-10 07:14:06 +0100

    More removals, regen benchmarks

--- facet-shapelike/src/tests.rs
+++ facet-shapelike/src/tests.rs
@@ -1,31 +1,13 @@
 use crate::shape_like::ShapeLike;
 use facet::Facet;
 use facet_args as args;
-use facet_kdl as kdl;
 use facet_xml as xml;
 
 #[derive(Facet)]
 #[repr(C)]
 struct TestStruct {
-    #[facet(kdl::property)]
     a: u32,
     b: bool,
-}
-
-#[derive(Facet)]
-struct KdlAttributes {
-    #[facet(kdl::child)]
-    child: u32,
-    #[facet(kdl::children)]
-    children: Vec<String>,
-    #[facet(kdl::property)]
-    prop: String,
-    #[facet(kdl::argument)]
-    arg: bool,
-    #[facet(kdl::arguments)]
-    args: Vec<i32>,
-    #[facet(kdl::node_name)]
-    name: String,
 }
 
 #[derive(Facet)]
@@ -52,16 +34,6 @@ struct ArgsAttributes {
 #[test]
 fn test_shape_serialization_roundtrip() {
     let shape = TestStruct::SHAPE;
-    let shape_like: ShapeLike = shape.into();
-    let json = facet_json::to_string(&shape_like).expect("Failed to serialize ShapeLike");
-    let deserialized: ShapeLike =
-        facet_json::from_str(&json).expect("Failed to deserialize ShapeLike");
-    facet_assert::assert_same!(shape_like, deserialized)
-}
-
-#[test]
-fn test_kdl_attributes_roundtrip() {
-    let shape = KdlAttributes::SHAPE;
     let shape_like: ShapeLike = shape.into();
     let json = facet_json::to_string(&shape_like).expect("Failed to serialize ShapeLike");
     let deserialized: ShapeLike =
