b949e7c48d730e6a469625fb69b8a605c7092229
Amos Wenger <amos@bearcove.eu>
2026-01-13 13:43:09 +0100

    test(facet-core): re-enable pointer type tests
    The pointer implementations have been migrated to the new VTable API,
    so these tests can now be re-enabled.

--- facet-core/tests/pointer.rs
+++ facet-core/tests/pointer.rs
@@ -1,39 +1,37 @@
-// TODO: Re-enable once pointer impls are migrated to new VTable API
-// use facet_core::{Facet, PointerType, Type};
-// use facet_testhelpers::test;
+use facet_core::{Facet, PointerType, Type};
 
-// #[test]
-// fn shape_name_string_slice_const_ptr() {
-//     let shape = <*const str as Facet>::SHAPE;
-//     match shape.ty {
-//         Type::Pointer(PointerType::Raw(vpt)) => {
-//             assert!(!vpt.mutable);
-//             assert_eq!(vpt.target.to_string(), "str");
-//         }
-//         _ => panic!("wrong type {:?}", shape.ty),
-//     }
-// }
+#[test]
+fn shape_name_string_slice_const_ptr() {
+    let shape = <*const str as Facet>::SHAPE;
+    match shape.ty {
+        Type::Pointer(PointerType::Raw(vpt)) => {
+            assert!(!vpt.mutable);
+            assert_eq!(vpt.target.to_string(), "str");
+        }
+        _ => panic!("wrong type {:?}", shape.ty),
+    }
+}
 
-// #[test]
-// fn shape_name_string_slice_mut_ptr() {
-//     let shape = <*mut str as Facet>::SHAPE;
-//     match shape.ty {
-//         Type::Pointer(PointerType::Raw(vpt)) => {
-//             assert!(vpt.mutable);
-//             assert_eq!(vpt.target.to_string(), "str");
-//         }
-//         _ => panic!("wrong type {:?}", shape.ty),
-//     }
-// }
+#[test]
+fn shape_name_string_slice_mut_ptr() {
+    let shape = <*mut str as Facet>::SHAPE;
+    match shape.ty {
+        Type::Pointer(PointerType::Raw(vpt)) => {
+            assert!(vpt.mutable);
+            assert_eq!(vpt.target.to_string(), "str");
+        }
+        _ => panic!("wrong type {:?}", shape.ty),
+    }
+}
 
-// #[test]
-// fn shape_name_string_slice_ref() {
-//     let shape = <&str as Facet>::SHAPE;
-//     match shape.ty {
-//         Type::Pointer(PointerType::Reference(vpt)) => {
-//             assert!(!vpt.mutable);
-//             assert_eq!(vpt.target.to_string(), "str");
-//         }
-//         _ => panic!("wrong type {:?}", shape.ty),
-//     }
-// }
+#[test]
+fn shape_name_string_slice_ref() {
+    let shape = <&str as Facet>::SHAPE;
+    match shape.ty {
+        Type::Pointer(PointerType::Reference(vpt)) => {
+            assert!(!vpt.mutable);
+            assert_eq!(vpt.target.to_string(), "str");
+        }
+        _ => panic!("wrong type {:?}", shape.ty),
+    }
+}
