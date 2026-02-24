f46e4eae565199a9eb08ccd7a24c0a92dd6340a4
Amos Wenger <amos@bearcove.eu>
2026-02-24 02:51:43 +0100

    test: make borrowed opaque derive check all-features safe

--- facet/tests/integration/derive.rs
+++ facet/tests/integration/derive.rs
@@ -488,21 +488,21 @@ fn opaque_arc() {
     }
 }
 
+#[allow(dead_code)]
+struct OpaqueBorrowedNotFacet;
+
+#[derive(Facet)]
+struct OpaqueBorrowed<'a> {
+    #[facet(opaque)]
+    inner: &'a OpaqueBorrowedNotFacet,
+}
+
 #[test]
 fn opaque_borrowed_reference() {
-    #[allow(dead_code)]
-    struct NotFacet;
+    let value = OpaqueBorrowedNotFacet;
+    let wrapped = OpaqueBorrowed { inner: &value };
 
-    #[derive(Facet)]
-    struct Borrowed<'a> {
-        #[facet(opaque)]
-        inner: &'a NotFacet,
-    }
-
-    let value = NotFacet;
-    let wrapped = Borrowed { inner: &value };
-
-    let shape = Borrowed::SHAPE;
+    let shape = OpaqueBorrowed::SHAPE;
     match shape.ty {
         Type::User(UserType::Struct(sk)) => {
             assert_eq!(sk.fields.len(), 1);
