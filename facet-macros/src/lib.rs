2055339a55eddee1f4b7386a98f387066718a4a7
Amos Wenger <amos@bearcove.eu>
2026-03-03 18:47:47 +0100

    docs: migrate crate docs/readmes to cargo-reedme

--- facet-macros/src/lib.rs
+++ facet-macros/src/lib.rs
@@ -1,4 +1,62 @@
-#![doc = include_str!("../README.md")]
+//! # facet-macros
+//!
+//! [![Coverage Status](https://coveralls.io/repos/github/facet-rs/facet-macros/badge.svg?branch=main)](https://coveralls.io/github/facet-rs/facet?branch=main)
+//! [![crates.io](https://img.shields.io/crates/v/facet-macros.svg)](https://crates.io/crates/facet-macros)
+//! [![documentation](https://docs.rs/facet-macros/badge.svg)](https://docs.rs/facet-macros)
+//! [![MIT/Apache-2.0 licensed](https://img.shields.io/crates/l/facet-macros.svg)](./LICENSE)
+//! [![Discord](https://img.shields.io/discord/1379550208551026748?logo=discord&label=discord)](https://discord.gg/JhD7CwCJ8F)
+//!
+//! Implements the `Facet` derive macro for facet. Uses [unsynn](https://crates.io/crates/unsynn) to provide fast compilation times.
+//!
+//! ## Sponsors
+//!
+//! Thanks to all individual sponsors:
+//!
+//! <p> <a href="https://github.com/sponsors/fasterthanlime">
+//! <picture>
+//! <source media="(prefers-color-scheme: dark)" srcset="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/github-dark.svg">
+//! <img src="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/github-light.svg" height="40" alt="GitHub Sponsors">
+//! </picture>
+//! </a> <a href="https://patreon.com/fasterthanlime">
+//!     <picture>
+//!     <source media="(prefers-color-scheme: dark)" srcset="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/patreon-dark.svg">
+//!     <img src="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/patreon-light.svg" height="40" alt="Patreon">
+//!     </picture>
+//! </a> </p>
+//!
+//! ...along with corporate sponsors:
+//!
+//! <p> <a href="https://aws.amazon.com">
+//! <picture>
+//! <source media="(prefers-color-scheme: dark)" srcset="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/aws-dark.svg">
+//! <img src="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/aws-light.svg" height="40" alt="AWS">
+//! </picture>
+//! </a> <a href="https://zed.dev">
+//! <picture>
+//! <source media="(prefers-color-scheme: dark)" srcset="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/zed-dark.svg">
+//! <img src="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/zed-light.svg" height="40" alt="Zed">
+//! </picture>
+//! </a> <a href="https://depot.dev?utm_source=facet">
+//! <picture>
+//! <source media="(prefers-color-scheme: dark)" srcset="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/depot-dark.svg">
+//! <img src="https://github.com/facet-rs/facet/raw/main/static/sponsors-v3/depot-light.svg" height="40" alt="Depot">
+//! </picture>
+//! </a> </p>
+//!
+//! ...without whom this work could not exist.
+//!
+//! ## Special thanks
+//!
+//! The facet logo was drawn by [Misiasart](https://misiasart.com/).
+//!
+//! ## License
+//!
+//! Licensed under either of:
+//!
+//! - Apache License, Version 2.0 ([LICENSE-APACHE](https://github.com/facet-rs/facet/blob/main/LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
+//! - MIT license ([LICENSE-MIT](https://github.com/facet-rs/facet/blob/main/LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+//!
+//! at your option.
 
 #[proc_macro_derive(Facet, attributes(facet))]
 pub fn facet_macros(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
