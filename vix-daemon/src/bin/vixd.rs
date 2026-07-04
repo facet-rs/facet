a23bce273aea73b54bf2597a541ef952e1cc2414
Amos Wenger <amos@bearcove.eu>
2026-07-04 09:06:05 +0200

    Format vixd env filter

--- vix-daemon/src/bin/vixd.rs
+++ vix-daemon/src/bin/vixd.rs
@@ -11,8 +11,7 @@ use vix_daemon::{DaemonDispatcher, DaemonService};
 async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .init();
 
